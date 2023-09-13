0480a83aaf6951e6bd268f1afd265fdeeb1f9372
Ronald Sancho <ronalsancho@gmail.com>
2023-09-12 18:03:08 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const ApiUrl = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+   try{
+    const result = await axios.get(ApiUrl +"/random");
+    res.render("index.ejs",{
+        secret: result.data.secret,
+        user: result.data.username,
+    });
+   }catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
